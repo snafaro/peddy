6b82a29a2fce6933703e127e5d72006176f70198
shahriar nafis <snafaro02@gmail.com>
2025-07-05 01:03:33 +0600

    category based pets load and show

--- script/peddy.js
+++ script/peddy.js
@@ -12,12 +12,30 @@ const showCategoryBtn = (arr) =>{
        const button = document.createElement('button');
        button.classList = "btn flex justify-center items-center gap-2 py-10 px-10"
        button.innerHTML = `
-            <img class="w-10" src="${item.category_icon}"/>
-            <span class="text-2xl font-bold">${item.category}</span>
+                <img class="w-10" src="${item.category_icon}"/>
+                <span class="text-2xl font-bold">${item.category}</span> 
+
        `;
+       button.addEventListener("click",(e)=>{
+            document.querySelectorAll('#categoryBtnContainer button').forEach(btn => {
+                btn.classList = "btn flex justify-center items-center gap-2 py-10 px-10";
+            });
+
+            // Add active class to the clicked button
+            e.currentTarget.classList = "btn flex justify-center items-center gap-2 py-10 px-10 rounded-full bg-[#0E7A811A] border-[#0E7A81]";
+            loadCategoryPets(`${item.category}`);
+       });
        container.append(button); 
     });
 };
+
+// pets load and show by category
+const loadCategoryPets = async(category) =>{
+    const url = `https://openapi.programming-hero.com/api/peddy/category/${category}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    showPets(data.data);
+}
 
 // pets load and show
 const loadAllPets = async() =>{
@@ -27,26 +45,31 @@ const loadAllPets = async() =>{
     showPets(data.pets);
 };
 
-const showPets = async(pets) =>{
+const showPets = (pets) =>{
     const petsCardContainer = document.getElementById('petsCardContainer');
+    petsCardContainer.innerHTML = "";
     pets.forEach((pet) =>{
         const div = document.createElement('div');
         div.classList = "p-5 border border-[#1313131A] rounded"
         div.innerHTML = `
-            <div class="h-[200px] w-full">
+            <div class="lg:h-[200px] md:h-[400px] w-full">
                 <img class="h-full w-full object-cover rounded" src="${pet.image}" />
             </div>
             <div class="border-b border-[#1313131A] py-4 mb-4">
                 <h2 class="font-bold text-[20px]">${pet.pet_name}</h2>
-                <p class="text-[#131313B3]">Breed : ${pet.breed}</p>
-                <p class="text-[#131313B3]">Birth : ${pet.date_of_birth}</p>
-                <p class="text-[#131313B3]">Gender : ${pet.gender}</p>
-                <p class="text-[#131313B3]">Price : ${pet.price}</p>
+                <p class="text-[#131313B3]">Breed : ${pet.breed ? pet.breed : "Not Found"}</p>
+                <p class="text-[#131313B3]">Birth : ${pet.date_of_birth ? pet.date_of_birth : "Not Found"}</p>
+                <p class="text-[#131313B3]">Gender : ${pet.gender ? pet.gender : "Not Found"}</p>
+                <p class="text-[#131313B3]">Price : $${pet.price ? pet.price : "Not Found"}</p>
             </div>
-            <div>
-            <button class="btn">Like</button>
-            <button class="btn text-[#0E7A81]">Adopt</button>
-            <button class="btn text-[#0E7A81]">Details</button>
+            <div class="flex items-center justify-center gap-4">
+            <button class="btn flex-1">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+                </svg>
+            </button>
+            <button class="btn text-[#0E7A81] flex-1">Adopt</button>
+            <button class="btn text-[#0E7A81] flex-1">Details</button>
             </div>
         `;
         petsCardContainer.append(div);
