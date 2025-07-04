d45a36bbb2f5185612c0bf928140a76b49db59ff
shahriar nafis <snafaro02@gmail.com>
2025-07-04 17:11:18 +0600

    category button load dynamically

--- script/peddy.js
+++ script/peddy.js
@@ -1,1 +1,22 @@
-console.log("peddy is here");
+// category button load and show
+const loadCategoryBtn = async() =>{
+    const url = "https://openapi.programming-hero.com/api/peddy/categories";
+    const res = await fetch(url);
+    const data = await res.json();
+    showCategoryBtn(data.categories);
+};
+
+const showCategoryBtn = (arr) =>{
+    const container = document.getElementById('categoryBtnContainer');
+    arr.forEach((item) => {
+       const button = document.createElement('button');
+       button.classList = "btn flex justify-center items-center gap-2 py-10 px-10"
+       button.innerHTML = `
+            <img class="w-10" src="${item.category_icon}"/>
+            <span class="text-2xl font-bold">${item.category}</span>
+       `;
+       container.append(button); 
+    });
+};
+
+loadCategoryBtn();
